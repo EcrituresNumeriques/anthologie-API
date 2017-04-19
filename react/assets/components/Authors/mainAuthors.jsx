a075ffb7648f6710e1b0c0d09186dfda646990a3
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-04-19 14:57:03 -0400

    fetch authors

--- react/assets/components/Authors/mainAuthors.jsx
+++ react/assets/components/Authors/mainAuthors.jsx
@@ -4,14 +4,34 @@ import Router, { Link, RouteHandler } from 'react-router';
 // components
 
 export default class mainAuthors extends Component {
+
+
   constructor(props) {
     super(props);
+    this.authors = [{name:'loading'}];
+    this.fetchAuthors();
   }
+
+   fetchAuthors(){
+     let that = this;
+     fetch('/api/v1/authors')
+     .then(function(response){
+       return response.json();
+     })
+     .then(function(json){
+       that.authors = json;
+       that.forceUpdate();
+     });
+   }
+
 
   render() {
 
     return (
-      <main>list of all authors</main>
+      <main>
+        <h1>list of all authors</h1>
+        {this.authors.map((author,i)=>(<p key={i}>{author.name}</p>))}
+        </main>
     );
   }
 }
