f5c1777fd33167db6a873a79244554107c8ec256
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-04-25 21:43:51 -0400

    policies ownsThis

---
+++ react/api/policies/ownsThis.js
@@ -0,0 +1,29 @@
+/**
+ * sessionAuth
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to allow any authenticated user
+ *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
+ * @docs        :: http://sailsjs.org/#!/documentation/concepts/Policies
+ *
+ */
+module.exports = function(req, res, next) {
+
+  // User is allowed, proceed to the next policy,
+  // or if this is the last policy, the controller
+
+  if (req.session.user.isAdmin) {
+    return next();
+  }
+  else{
+    // Use existing req.options.where, or initialize it to an empty object
+    req.options.where = req.options.where || {};
+    // Set the default `userId` for "find" and "update" blueprints
+    req.options.where.id_user = req.session.user.id;
+    next();
+  }
+
+  // User is not allowed
+  // (default res.forbidden() behavior can be overridden in `config/403.js`)
+  return res.forbidden('You need to be logged in to perform this action.');
+};
