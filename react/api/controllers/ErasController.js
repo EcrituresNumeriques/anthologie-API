9e8e09f67d777c0fbd5ca2ecc00fbfb923c5fb7c
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-05-17 12:11:56 -0400

    Controllers

---
+++ react/api/controllers/ErasController.js
@@ -0,0 +1,8 @@
+/**
+ * ErasController.js
+ *
+ * @description :: Server-side logic for managing subscriptions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {};
