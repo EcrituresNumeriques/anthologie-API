803f188be68d7be4eab00a64ea81246eb75bc9e4
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-05-26 23:42:07 -0400

    gps for city

--- react/assets/components/Cities/specificCity.jsx
+++ react/assets/components/Cities/specificCity.jsx
@@ -9,7 +9,7 @@ export default class specificCity extends Component {
 
   constructor(props) {
     super(props);
-    this.city =   {"translations": [{"id_city_translation": 0,"id_city": 0,"id_user": 0,"id_group": 0,"id_language": 1,"name": "loading",}],"cityities": [],"images": [],  "entities": [],"id_user": {"id_user": 0,"displayName": "Admin","institution": "Anthologie","country": "Canada","createdAt": "2017-05-24T14:18:59.000Z","updatedAt": "2017-05-25T06:30:46.000Z"},"id_city": 0,"born": null,"born_range": null,"died": null,"died_range": null,"activity": null,"activity_range": null,"createdAt": "2017-05-25T07:38:26.000Z","updatedAt": "2017-05-25T07:38:26.000Z"};
+    this.city =   {"translations": [{"id_city_translation": 0,"id_city": 0,"id_user": 0,"id_group": 0,"id_language": 1,"name": "loading",}],"cityities": [],"images": [],  "entities": [],"id_user": {"id_user": 0,"displayName": "Admin","institution": "Anthologie","country": "Canada","createdAt": "2017-05-24T14:18:59.000Z","updatedAt": "2017-05-25T06:30:46.000Z"},"id_city": 0,"GPS":null,"createdAt": "2017-05-25T07:38:26.000Z","updatedAt": "2017-05-25T07:38:26.000Z"};
     this.fetchAPI();
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -25,6 +25,7 @@ export default class specificCity extends Component {
       })
       .then(function(json){
         that.city = json;
+        that.refs.gps.value = json.GPS;
         that.forceUpdate();
         return null;
       });
@@ -34,7 +35,7 @@ export default class specificCity extends Component {
     e.preventDefault();
     let that = this;
     //get name and family
-    let corps = {name:this.refs.name.value,family:this.refs.family.value}
+    let corps = {GPS:this.refs.gps.value}
     fetch("/api/v1/cities/"+that.props.params.id,
     {
         method: "POST",
@@ -46,6 +47,8 @@ export default class specificCity extends Component {
       return res.json()})
     .then(function(data){
       that.city = data;
+      //console.log(that.refs.gps);
+      that.refs.gps.value = data.GPS;
       that.forceUpdate();
       return null})
     .catch(function(error){return error})
@@ -66,6 +69,7 @@ export default class specificCity extends Component {
         <h1>{this.city.translations.map(a => a.name).join(" / ")}</h1>
           <form onSubmit={this.handleSubmit}>
             <div className="inputContainerLanguage"><label>ID city : </label><input type="text" value={this.city.id_city} disabled="true"/></div>
+            <div className="inputContainerLanguage"><label>GPS : </label><input placeholder="ex : 45.4991117,-73.6181167" type="text" ref="gps" defaultValue={this.city.GPS} disabled={readOnly} /></div>
             <div className="inputContainerLanguage"><label>created at : </label><input type="text" value={this.city.createdAt} disabled="true"/></div>
             <div className="inputContainerLanguage"><label>updated at : </label><input type="text" value={this.city.updatedAt} disabled="true"/></div>
             {this.city.id_user && <div className="inputContainerLanguage"><label>Owner : </label><input type="text" value={'['+this.city.id_user.institution+'] ' + this.city.id_user.displayName} disabled="true"/></div>}
