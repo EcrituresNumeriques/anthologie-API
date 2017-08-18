998d5572ef3e71a93bf9527a818ca197e9b43b22
PookMook <arthur.juchereau@gmail.com>
2017-08-17 21:57:43 -0400

    not in store notes

--- src/assets/components/Notes/mainNotes.jsx
+++ src/assets/components/Notes/mainNotes.jsx
@@ -34,10 +34,12 @@ export default class mainNotes extends Component {
 
     return (
       <main>
+        <div>
         <h1>list of all notes</h1>
         <h6>anthologia.ecrituresnumeriques.ca/api/v1/notes</h6>
         {store.getState().loggedIn && <Link to="/notes/new" className="addto" activeStyle={{ color: 'black' }}>add new note</Link>}
         {this.notes.map((note,i)=>(<Link to={"/notes/"+note.id_note} key={"note"+note.id_note} id={note.id_note}>{note.title}</Link>))}
+        </div>
         </main>
     );
   }
