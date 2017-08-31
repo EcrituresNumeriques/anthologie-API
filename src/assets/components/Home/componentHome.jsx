ac21d678c8c46418d3484334a89f61cb76f46e33
PookMook <arthur.juchereau@gmail.com>
2017-08-31 16:41:24 -0400

    home component like other component layout wise

--- src/assets/components/Home/componentHome.jsx
+++ src/assets/components/Home/componentHome.jsx
@@ -33,28 +33,30 @@ export default class ComponentHome extends Component {
   render() {
     return (
       <main id="homeView">
-        <section id="welcome">
-          <h1>Welcome to the Anthologia</h1>
-          <p>You can find the source code and contribute on <a href="https://github.com/EcrituresNumeriques/anthologie-API" target="_blank">Github</a>.</p>
-          <p>You can also contribute by importing texts from perseus and adding/aligning translations.</p>
-        </section>
-        <section id="find">
-          <h1>Find something on the platform</h1>
-          <input list="items" type="text" id="selector"/>
-          <button onClick={()=>this.search()}>go</button>
-          <datalist id="items">
-            {this.authors.map((author,i)=>(<option key={"authorSelect"+author.id_author} value={'/authors/'+author.id_author}>author : {author.translations.map(a => a.name).join(" / ")}</option>))}
-            {this.cities.map((city,i)=>(<option key={"citySelect"+city.id_city} value={'/cities/'+city.id_city}>city : {city.translations.map(a => a.name).join(" / ")}</option>))}
-            {this.entities.map((entity,i)=>(<option key={"entitiesSelect"+entity.id_entity} value={'/entities/'+entity.id_entity}>entity : {entity.title}</option>))}
-            {this.keywords.map((keyword,i)=>(<option key={"keywordsSelect"+keyword.id_keyword} value={'/keywords/'+keyword.id_keyword}>keyword : {keyword.translations.map(a => a.title).join(" / ")}</option>))}
-          </datalist>
-        </section>
-        <section id="perseus">
-          <h1>Import from perseus</h1>
-          <input type="text" id="perseusURI" defaultValue="http://data.perseus.org/citations/urn:cts:greekLit:tlg7000.tlg001.perseus-grc1:5.6"/>
-          <button onClick={()=>this.perseus()}>go</button>
-        </section>
-        <Links/>
+        <aside><Links/></aside>
+        <main>
+          <section id="welcome">
+            <h1>Welcome to the Anthologia</h1>
+            <p>You can find the source code and contribute on <a href="https://github.com/EcrituresNumeriques/anthologie-API" target="_blank">Github</a>.</p>
+            <p>You can also contribute by importing texts from perseus and adding/aligning translations.</p>
+          </section>
+          <section id="find">
+            <h1>Find something on the platform</h1>
+            <input list="items" type="text" id="selector"/>
+            <button onClick={()=>this.search()}>go</button>
+            <datalist id="items">
+              {this.authors.map((author,i)=>(<option key={"authorSelect"+author.id_author} value={'/authors/'+author.id_author}>author : {author.translations.map(a => a.name).join(" / ")}</option>))}
+              {this.cities.map((city,i)=>(<option key={"citySelect"+city.id_city} value={'/cities/'+city.id_city}>city : {city.translations.map(a => a.name).join(" / ")}</option>))}
+              {this.entities.map((entity,i)=>(<option key={"entitiesSelect"+entity.id_entity} value={'/entities/'+entity.id_entity}>entity : {entity.title}</option>))}
+              {this.keywords.map((keyword,i)=>(<option key={"keywordsSelect"+keyword.id_keyword} value={'/keywords/'+keyword.id_keyword}>keyword : {keyword.translations.map(a => a.title).join(" / ")}</option>))}
+            </datalist>
+          </section>
+          <section id="perseus">
+            <h1>Import from perseus</h1>
+            <input type="text" id="perseusURI" defaultValue="http://data.perseus.org/citations/urn:cts:greekLit:tlg7000.tlg001.perseus-grc1:5.6"/>
+            <button onClick={()=>this.perseus()}>go</button>
+          </section>
+        </main>
       </main>
     );
   }
