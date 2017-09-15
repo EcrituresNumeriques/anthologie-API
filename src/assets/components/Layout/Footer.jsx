d0d7c8fff4bb27f18b5e3f810cd4c948728c51b6
PookMook <arthur.juchereau@gmail.com>
2017-09-15 14:28:48 -0400

    fix footer

--- src/assets/components/Layout/Footer.jsx
+++ src/assets/components/Layout/Footer.jsx
@@ -8,7 +8,15 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <div>Anthologia palatina - <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />All the texts on this platform are available under the <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</div>
+        <div>
+          <h3>Anthologia palatina</h3>
+          <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">
+            <img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+          </a>
+          <br />
+          All the texts on this platform are available under the <br />
+          <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License.</a>
+        </div>
       </footer>
     );
   }
