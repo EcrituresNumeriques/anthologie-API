7bde8f883971b191f5a8327c03bf6443d3615365
PookMook <arthur.juchereau@gmail.com>
2018-02-08 17:46:42 -0500

    cleanup

--- src/assets/Redux/store.js
+++
@@ -1,5 +0,0 @@
-//Redux store
-import { createStore } from 'redux'
-import {login} from './Reducers/Login.js'
-
-export let store = createStore(login);
--- src/assets/helpers/displayLang.jsx
+++
@@ -1,5 +0,0 @@
-export let displayLang = function(lang){
-  if(!lang){return undefined};
-  let string = lang.family+" / "+lang.name;
-  return string;
-}
